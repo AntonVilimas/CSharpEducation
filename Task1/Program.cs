634ad67c1ebfc0774d3a85a7f771d1b25668df79
AntonPC <AntonPC@ANTONPC>
2024-07-24 21:10:45 +0400

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://docs.yandex.ru/docs/view?url=ya-browser%3A%2F%2F4DT1uXEPRrJRXlUFoewruLnuCBnKLEPYydnLmFpsStc2n7-X6lXGf80PHtNyrrnhtvOse1dozTN9bTpedYuMJ8dQFaROa-eXlpJB_6fXMA2MHqqPMFhcORv9eQ1r_cvjV5CA0sJKbpxb6n_EcMvz7A%3D%3D%3Fsign%3DZQ-42DoMFOIxAHx_Q6NsLRvhZXWsE4eXMSKiBcFAbbg%3D&name=Обучение%20C%23.%20Урок%201.%20Введение.PPTX&nosw=1
+const string text = "HelloWorld";
+Console.WriteLine (text);
